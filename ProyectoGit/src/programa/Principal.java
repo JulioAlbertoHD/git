25d4872da9cde4a47fe425ba0e179fbc24e84ccb
alber <alber@DESKTOP-HKQ2KAL>
2024-03-11 21:01:55 +0100

    v1.1 cambio mensaje

--- ProyectoGit/src/programa/Principal.java
+++ ProyectoGit/src/programa/Principal.java
@@ -3,7 +3,7 @@ package programa;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Hola, la tarde está muy entretenida");
 
 	}
 
